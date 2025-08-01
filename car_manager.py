6c37666bc1cae914b66b3f7fab5f41cca157d94d
Andrés <andresre@icloud.com>
2025-08-01 17:36:02 -0600

    Final Commit

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,38 @@
+import random
+from turtle import Turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
-MOVE_INCREMENT = 10
+MOVE_INCREMENT = 5
 
 
 class CarManager:
-    pass
+
+    def __init__(self):
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def create_car(self):
+        new_car = Turtle("square")
+        new_car.penup()
+        new_car.color(random.choice(COLORS))
+        new_car.shapesize(stretch_wid=1, stretch_len=2)
+        y_pos = random.randint(-270, 250)
+        new_car.goto(x=270, y=y_pos)
+        self.all_cars.append(new_car)
+
+    def move_car(self):
+        for car in self.all_cars:
+            new_x = car.xcor() - self.car_speed
+            car.goto(new_x, car.ycor())
+
+    def level_up(self):
+        for car in self.all_cars:
+            car.hideturtle()
+            car.clear()
+            car.goto(1000, 1000)
+        self.all_cars = []
+        self.car_speed += MOVE_INCREMENT
+
+
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import time
 from turtle import Screen
 from player import Player
@@ -7,9 +8,12 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.title("Turtle Crossing Game")
 
 player = Player()
 scoreboard = Scoreboard()
+car_manager = CarManager()
+
 screen.listen()
 screen.onkeypress(player.move, "Up")
 
@@ -18,5 +22,18 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
 
+    if random.randint(1, 6) == 1:
+        car_manager.create_car()
+
+    car_manager.move_car()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
     if player.restart_pos():
         scoreboard.increase_score()
+        car_manager.level_up()
+
+screen.exitonclick()
